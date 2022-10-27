e5be334cea677fe119aef93931fce3d311c38b9b
danielmartinsheehan <daniel.martin.sheehan@gmail.com>
2022-10-26 22:58:23 -0400

    finished course

---
+++ Alexandria/catalog/management/commands/authors.py
@@ -0,0 +1,21 @@
+
+from django.core.management.base import BaseCommand
+from catalog.models import Author
+
+
+class Command(BaseCommand):
+    help = 'Lists authors in the Library Alexandria'
+
+    def handle(self, *args, **options):
+        for author in Author.objects.all():
+            output = [f'{author.last_name}']
+
+            if author.first_name:
+                output.append(f', {author.first_name}')
+
+            output.append(f' ({author.birth_year})')
+
+            if author.picture:
+                output.append(f' {author.picture.name}')
+
+            self.stdout.write(''.join(output))
